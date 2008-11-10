4feef8d399389181c6a5efdd61193fdfe7d06434
whit <whit@openplans.org>
2008-11-10 15:11:59 -0600

    add basic paster layout and buildout.cfg

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='psid',
+      version=version,
+      description="simple spatial index server",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Sean Gillies and Whit Morriss',
+      author_email='whit [at] opengeo.org',
+      url='',
+      license='BSDish',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'PasteDeploy',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [paste.app_factory]
+      main = psid.wsgiapp:make_app
+      """,
+      )

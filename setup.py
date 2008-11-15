1521d04078240b6e3eed58f49553bead0bf41d6a
whit <whit@openplans.org>
2008-11-15 01:10:29 -0600

    import command after minilib

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ import os
 try:
     import paver.command
 except ImportError:
-    import paver.command
     if os.path.exists("paver-minilib.zip"):
         import sys
         sys.path.insert(0, "paver-minilib.zip")
-
+    import paver.command
 paver.command.main()
